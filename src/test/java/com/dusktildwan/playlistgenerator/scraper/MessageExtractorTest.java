51e79645d4505dfc4cd30ffafd74d62a3e7acfc6
dusktilldwan <dwan.williams.t@gmail.com>
2025-03-26 23:28:00 -0500

    -r . moved initial scraper logic to own package. - Changed rootproject name. - added ReadMe.md - added .env & .envrc to gitignore

--- src/test/java/com/dusktildwan/playlistgenerator/scraper/MessageExtractorTest.java
+++ src/test/java/com/dusktildwan/playlistgenerator/scraper/MessageExtractorTest.java
@@ -1,4 +1,4 @@
-package com.dusktildwan.playlistgenerator;
+package com.dusktildwan.playlistgenerator.scraper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/dusktildwan/playlistgenerator/scraper/YouTubeLinkParserTest.java
+++ src/test/java/com/dusktildwan/playlistgenerator/scraper/YouTubeLinkParserTest.java
@@ -1,4 +1,4 @@
-package com.dusktildwan.playlistgenerator;
+package com.dusktildwan.playlistgenerator.scraper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
