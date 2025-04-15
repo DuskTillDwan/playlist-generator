1d9d0ec76cec29c66cd01ad0635c43d933776e0f
dusktilldwan <dwan.williams.t@gmail.com>
2025-04-14 19:40:57 -0500

    -r ! - added UploadService.java and UploadServiceTest.java - plan to move test data creation to test data factory class

---
+++ src/test/java/com/dusktildwan/playlistgenerator/util/PlatformDetectorTest.java
@@ -0,0 +1,40 @@
+package com.dusktildwan.playlistgenerator.util;
+
+import org.junit.jupiter.api.Test;
+
+import static com.dusktildwan.playlistgenerator.util.PlatformDetector.detectPlatform;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlatformDetectorTest {
+
+    @Test
+    void detectsSpotifyFromSpotifyComUrl() {
+        assertEquals("SPOTIFY", detectPlatform("https://open.spotify.com/track/123"));
+    }
+
+    @Test
+    void detectsSpotifyFromSpotifyLinkUrl() {
+        assertEquals("SPOTIFY", detectPlatform("https://spotify.link/some-id"));
+    }
+
+    @Test
+    void detectsYoutubeFromYoutubeComUrl() {
+        assertEquals("YOUTUBE", detectPlatform("https://www.youtube.com/watch?v=abc"));
+    }
+
+    @Test
+    void detectsYoutubeFromYoutubeUrl() {
+        assertEquals("YOUTUBE", detectPlatform("https://youtu.be/xyz"));
+    }
+
+    @Test
+    void detectsSoundCloudFromUrl() {
+        assertEquals("SOUNDCLOUD", detectPlatform("https://soundcloud.com/artist/track"));
+    }
+
+    @Test
+    void returnsUnknownForUnsupportedUrl() {
+        assertEquals("UNKNOWN", detectPlatform("https://example.com/whatever"));
+    }
+
+}
