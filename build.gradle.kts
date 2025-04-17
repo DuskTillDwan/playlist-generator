55c8666b75cd12f706726ab7e73485c96f397d96
dusktilldwan <dwan.williams.t@gmail.com>
2025-04-17 14:12:52 -0500

    -r . added common module for shared dependencies. Removed subprojects from root build.gradle.kts per gradle recommendations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,24 +12,3 @@ allprojects {
 		mavenCentral()
 	}
 }
-
-subprojects {
-	apply(plugin = "java")
-	apply(plugin = "io.spring.dependency-management")
-
-	java {
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of(21))
-		}
-	}
-
-	dependencies {
-		// Add common test dependencies
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	}
-
-	tasks.test {
-		useJUnitPlatform()
-	}
-}
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.4.2"
+    id("java-library")
+    id("org.springframework.boot") version "3.4.2" apply false
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-apply(from = File("${project.rootDir}/gradle/integration.gradle.kts")) // rename if needed
+//apply(from = File("${project.rootDir}/gradle/integration.gradle.kts")) // rename if needed
 
 group = "com.dusktildwan"
 version = "0.0.1-SNAPSHOT"
@@ -22,24 +22,19 @@ configurations {
 }
 
 dependencies {
-    implementation(project(":common"))
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.flywaydb:flyway-database-postgresql")
+    api("org.springframework.boot:spring-boot-starter-web")
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("com.google.code.gson:gson:2.12.1")
+    api("org.postgresql:postgresql")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.check {
-    dependsOn("integrationTest")
-}
+//tasks.check {
+//    dependsOn("integrationTest")
+//}
