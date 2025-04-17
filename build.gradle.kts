df152f4664cb0e64bcb061b81754d329ee42a4e2
dusktilldwan <dwan.williams.t@gmail.com>
2025-04-16 19:19:21 -0500

    -r . - moved original project under new module batch-service. may be renamed in the future.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,35 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.2"
+	id("org.springframework.boot") version "3.4.2" apply false
 	id("io.spring.dependency-management") version "1.1.7"
 }
-
-apply(from = File("./gradle/integration.gradle.kts")) // rename if needed
 
 group = "com.dusktildwan"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(21))
+allprojects {
+	repositories {
+		mavenCentral()
 	}
 }
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
+subprojects {
+	apply(plugin = "java")
+	apply(plugin = "io.spring.dependency-management")
+
+	java {
+		toolchain {
+			languageVersion.set(JavaLanguageVersion.of(21))
+		}
 	}
-}
 
-repositories {
-	mavenCentral()
-}
+	dependencies {
+		// Add common test dependencies
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	}
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("com.google.code.gson:gson:2.12.1")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-database-postgresql")
-	implementation("org.postgresql:postgresql")
-
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-
-	testImplementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
-
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-	useJUnitPlatform()
-}
-
-tasks.check {
-	dependsOn("integrationTest")
+	tasks.test {
+		useJUnitPlatform()
+	}
 }
